f5621337083e07fbb54fb0de809365f905bff71a
Luc Wicki <Luc_Wicki@sluz.ch>
2022-07-07 10:04:41 +0200

    commit correction

--- app/Controllers/VerleihController.php
+++ app/Controllers/VerleihController.php
@@ -3,30 +3,28 @@
     class VerleihController{
 
         public function erfassen(){
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $pdo = db();
-
-            $statement = $pdo->prepare("INSERT INTO `verleiheanmeldung` (Email) VALUES (:Email)");
-            $statement->bindParam(':Email', $_POST['Email'], PDO::PARAM_STR);
-
-            $statement->execute();
-
-            require 'app/Views/Verleih.view.php';
-            }
-        }
-
-        public function uebersicht(){
-
             
+          
+            require 'app/Views/Verleih.view.php';
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 $pdo = db();
     
                /*  $statement = $pdo->prepare("INSERT INTO `verleiheanmeldung` (Name) VALUES (:Name)");
                 $statement->bindParam(':Name', $_POST['Name'], PDO::PARAM_STR); */
 
+                $statement = $pdo->prepare("INSERT INTO `verleiheanmeldung` (Email) VALUES (:Email)");
+                $statement->bindParam(':Email', $_POST['Email'], PDO::PARAM_STR);
+
+                $statement->execute();
     
     
             }
+        }
+
+        public function uebersicht(){
+
+          
             /*
             $pdo = db();
 
