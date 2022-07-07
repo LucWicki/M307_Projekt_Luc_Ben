83bcec9589496020e5764b587bc360bea371fa54
Ben Streich <ben.streich@icloud.com>
2022-07-07 15:57:49 +0200

    Js Changes again

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,20 @@
-// Javascript
-console.info('JS geladen.');
+
+
+function berechnen(){
+
+    var frist = 480;
+    var select = document.getElementById('risikostufe');
+    var value = select.options[select.selectedIndex].value;
+
+    switch(value){
+
+        case 'sehr tief': frist+= 360; break;
+        case 'tief': frist+=180; break;
+        case 'hoch': frist-=120; break;
+        case 'sehr hoch': frist-=240; break;
+
+    }
+
+
+
+}
